25ac2c38ac47477a228e8dd2dd9665adbc78fc17
Borisova.albina@gmail.com <Borisova.albina@gmail.com@6c98e674-3fba-0178-029c-bbb524815497>
2013-05-12 08:54:45 +0000

    Создан новый проект для выделения особых точек комплексной сверткой и с помощю индекса Пуанкаре.

---
+++ SingularPointsExtraction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingularPointsExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //выделение точек дельты методом комплексной светки и с помощью индекса Пуанкаре
+            //пока пусто
+
+        }
+    }
+}
