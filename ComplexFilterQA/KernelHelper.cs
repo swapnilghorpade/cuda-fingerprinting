711fd6d49f167ce486487a3016318761c5d4c7c1
borisova.albina@gmail.com <borisova.albina@gmail.com@6c98e674-3fba-0178-029c-bbb524815497>
2013-05-19 15:01:28 +0000

    Add method MakeKernel(func, sixeX, sizeY)

--- ComplexFilterQA/KernelHelper.cs
+++ ComplexFilterQA/KernelHelper.cs
@@ -7,7 +7,7 @@ namespace ComplexFilterQA
     {
         public static int GetKernelSizeForGaussianSigma(double sigma)
         {
-            return 2*(int) Math.Ceiling(sigma*3.0f) + 1;
+            return 2 * (int)Math.Ceiling(sigma * 3.0f) + 1;
         }
 
         public static Complex[,] MakeComplexKernel(Func<int, int, double> realFunction,
@@ -31,10 +31,16 @@ namespace ComplexFilterQA
             return max;
         }
 
+        /// <summary>
+        /// Make squared kernel.
+        /// </summary>
+        /// <param name="function"></param>
+        /// <param name="size"></param>
+        /// <returns></returns>
         public static double[,] MakeKernel(Func<int, int, double> function, int size)
         {
-            double[,] kernel = new double[size,size];
-            int center = size/2;
+            double[,] kernel = new double[size, size];
+            int center = size / 2;
             double sum = 0;
             for (int x = -center; x <= center; x++)
             {
@@ -44,7 +50,7 @@ namespace ComplexFilterQA
                 }
             }
             // normalization
-            if (Math.Abs(sum) >0.0000001)
+            if (Math.Abs(sum) > 0.0000001)
                 for (int x = -center; x <= center; x++)
                 {
                     for (int y = -center; y <= center; y++)
@@ -55,16 +61,42 @@ namespace ComplexFilterQA
             return kernel;
         }
 
-    public static Complex[,] MakeComplexFromDouble(double[,] real, double[,] imaginary)
+        /// <summary>
+        /// Make rectangular kernel without normalization.
+        /// even and odd
+        /// </summary>
+        /// <param name="function"></param>
+        /// <param name="sizeX"></param>
+        /// <param name="sizeY"></param>
+        /// <returns></returns>
+        public static double[,] MakeKernel(Func<float, float, double> function, int sizeX, int sizeY)
+        {
+            double[,] kernel = new double[sizeX, sizeY];
+            int centerX = sizeX / 2;
+            int centerY = sizeY / 2;
+            float shiftX = (sizeX - 2 * centerX - 1) / 2f;
+            float shiftY = (sizeY - 2 * centerY - 1)/2f;
+
+            for (int x = -centerX; x <= centerX+2*shiftX; x++)
+            {
+                for (int y = -centerY; y <= centerY+2*shiftY; y++)
+                {
+                    kernel[centerX + x, centerY + y] = function(x-shiftX, y-shiftY);
+                }
+            }
+            return kernel;
+        }
+
+        public static Complex[,] MakeComplexFromDouble(double[,] real, double[,] imaginary)
         {
             int maxX = real.GetLength(0);
             int maxY = real.GetLength(1);
             Complex[,] result = new Complex[maxX, maxY];
-            for (int x = 0; x <maxX; x++)
+            for (int x = 0; x < maxX; x++)
             {
-                for (int y = 0; y <maxY; y++)
+                for (int y = 0; y < maxY; y++)
                 {
-                    result[x, y] = new Complex(real[x,y],imaginary[x,y]);
+                    result[x, y] = new Complex(real[x, y], imaginary[x, y]);
                 }
             }
             return result;
@@ -85,7 +117,7 @@ namespace ComplexFilterQA
             return result;
         }
 
-        public static double[,] Zip2D(double[,] arr1, double[,] arr2, Func<double,double,double> f)
+        public static double[,] Zip2D(double[,] arr1, double[,] arr2, Func<double, double, double> f)
         {
             var result = new double[arr1.GetLength(0), arr1.GetLength(1)];
             for (int x = 0; x < arr1.GetLength(0); x++)
@@ -128,7 +160,7 @@ namespace ComplexFilterQA
 
         public static U[,] Select2D<T, U>(this T[,] array, Func<T, U> f)
         {
-            var result = new U[array.GetLength(0),array.GetLength(1)];
+            var result = new U[array.GetLength(0), array.GetLength(1)];
 
             for (int x = 0; x < array.GetLength(0); x++)
             {
--- SingularPointsExtraction/ExtractSPComplexFiltering.cs
+++ SingularPointsExtraction/ExtractSPComplexFiltering.cs
@@ -22,10 +22,11 @@ namespace SingularPointsExtraction
             //ImageHelper.SaveArray(level2, "D:/1-2.bmp");
             //ImageHelper.SaveArray(level3, "D:/1-3.bmp");
 
-            //calculate response of 
-            double[,] forDelta3 = level3.Select2D((value, row, column) => (level0[row, level3.GetLength(1) - column - 1]));
+            //Calculate response of parabolic symmetry filter
+
+            double[,] forDelta3 = level3.Select2D((value, row, column) => (level3[row, level3.GetLength(1) - column - 1]));
   
-            Complex[,] response3 = Symmetry.EstimatePS(level3, 0.6d, 3.2d);                      
+            Complex[,] response3 = Symmetry.EstimatePS(level3, 1.6d, 3.2d);                      
             Complex[,] responseForDelta3 = Symmetry.EstimatePS(forDelta3, 0.6d, 3.2d);
             for (int i = 0; i < response3.GetLength(0); i++)
             {
@@ -34,14 +35,29 @@ namespace SingularPointsExtraction
                     double newMagnitude = response3[i, j].Magnitude * (1d - responseForDelta3[i, j].Magnitude);
                     response3[i, j] = Complex.FromPolarCoordinates(newMagnitude, response3[i,j].Phase);                    
                 }                
-            }            
-            //double[,] aaa = response3.Select2D((value, row, column) => (response3[row,column].Magnitude));
-            //ImageHelper.SaveArray(aaa, "D:/1-5.bmp");
+            }
 
-            Complex[,] z = GetZ(level3, 0.6d);
+            double[,] bbb = response3.Select2D((value, row, column) => (response3[row, column].Magnitude));
+            ImageHelper.SaveArray(bbb, "D:/1-4.bmp");
 
-            //var kernel2 = KernelHelper.MakeComplexKernel((x, y) => Gaussian.Gaussian2D(x, y, 1.5d) * x, (x, y) => Gaussian.Gaussian2D(x, y, 1.5d) * y, KernelHelper.GetKernelSizeForGaussianSigma(Sigma2));
+            //Compute a modified complex filter response(for level 3)
+            Complex[,] z = GetZ(level3, 1.6d);
+            double[,] magnitudeZ = z.Select2D((value,x,y)=>(z[x,y].Magnitude));
 
+            ImageHelper.SaveArray(magnitudeZ, "D:/1-3.bmp");
+            double[,] gaussians = KernelHelper.MakeKernel((x, y) => Gaussian.Gaussian2D(x, y, 1.5d), KernelHelper.GetKernelSizeForGaussianSigma(1.5d));
+            double[,] resultOfconvolve = ConvolutionHelper.Convolve(magnitudeZ, gaussians);
+            Complex[,] zc = response3.Select2D((value,x,y)=>(response3[x,y]*resultOfconvolve[x,y]));
+
+            double[,] bigGaussian = KernelHelper.MakeKernel((x, y) => Gaussian.Gaussian2D((float)x, (float)y, 11.7, 7), response3.GetLength(0), response3.GetLength(1));
+            Complex[,] gc = response3.Select2D((value, x, y) => (response3[x, y] * bigGaussian[x, y]));
+
+            Complex[,] modifiedResponse3 = gc.Select2D((value, x, y) => 0.5*(gc[x, y] + zc[x, y]));
+
+            
+            double[,] aaa = modifiedResponse3.Select2D((value, row, column) => (modifiedResponse3[row,column].Magnitude));
+            ImageHelper.SaveArray(aaa, "D:/1-5.bmp");
+            
             return null;
         }
 
