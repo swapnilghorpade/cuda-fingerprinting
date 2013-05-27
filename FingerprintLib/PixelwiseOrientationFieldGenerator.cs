3f6d85567828c95e9eb0f8d674ee8f4800108512
stanislav.sartasov@gmail.com <stanislav.sartasov@gmail.com@6c98e674-3fba-0178-029c-bbb524815497>
2013-05-27 16:49:31 +0000

    - fixed error in pixelwise field generator

--- FingerprintLib/PixelwiseOrientationFieldGenerator.cs
+++ FingerprintLib/PixelwiseOrientationFieldGenerator.cs
@@ -40,8 +40,12 @@ namespace FingerprintLib
             Gxy = ConvolutionHelper.Convolve(Gxy, kernelAv);
             Gyy = ConvolutionHelper.Convolve(Gyy, kernelAv);
 
-            var angles = Gxx.Select2D((gxx, row, column) => 0.5*Math.Atan2(gxx - Gyy[row, column], 2.0*Gxy[row, column]));
+            var angles = Gxx.Select2D((gxx, row, column) => 0.5*Math.Atan2( 2.0*Gxy[row, column],gxx - Gyy[row, column]));
+
+            var min = KernelHelper.Min2d(angles);
+            
             angles = angles.Select2D(angle => angle <= 0 ? angle + Math.PI/2 : angle - Math.PI/2);
+            //ImageHelper.SaveFieldAbove(bytes, angles, "C:\\temp\\orField.png");
             //ImageHelper.SaveArray(angles, "C:\\temp\\angles.png");
             return angles;
         }
