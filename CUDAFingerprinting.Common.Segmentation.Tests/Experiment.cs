ea173edb894fe4916f09ce0e9471421291fd5725
chugaevatv@gmail.com <chugaevatv@gmail.com@6c98e674-3fba-0178-029c-bbb524815497>
2013-07-03 13:40:53 +0000

    Create Average method, Segmentation and test for it.

---
+++ CUDAFingerprinting.Common.Segmentation.Tests/Experiment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CUDAFingerprinting.Common.Segmentation.Tests
+{
+    [TestClass]
+    public class Experiment
+    {
+        [TestMethod]
+        public void ExperimentMethod()
+        {
+            double[,] img = ImageHelper.LoadImage(Resources.TestImage);
+            double[,] resultImg;
+            int minValue = 5;
+            int maxValue = 24;
+
+            for (int windowRadius = minValue; windowRadius < maxValue; windowRadius++)
+            {
+               resultImg = Segmentator.Segmetator(img, windowRadius);
+               ImageHelper.SaveArray(resultImg, Path.GetTempPath() + "resultImg" + windowRadius + ".png");
+            }
+        }
+    }
+}
---
+++ CUDAFingerprinting.Common.Segmentation/Segmentator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CUDAFingerprinting.Common.OrientationField;
+
+namespace CUDAFingerprinting.Common.Segmentation
+{
+    public static class Segmentator
+    {
+        public static double[,] Segmetator(double[,] img, int windowRadius)
+        {
+            int[,] xGradients = OrientationFieldGenerator.GenerateXGradients(img.Select2D(a => (int)a));
+            int[,] yGradients = OrientationFieldGenerator.GenerateYGradients(img.Select2D(a => (int)a));
+            double[,] magnitudes = xGradients.Select2D((x, y, value) => Math.Sqrt(xGradients[x, y] * xGradients[x, y] + yGradients[x, y] * yGradients[x, y]));
+            double averege = KernelHelper.Average(magnitudes);
+            double[,] window = new double[windowRadius * 2 + 1, windowRadius * 2 + 1];
+            double[,] result = new double[img.GetLength(0), img.GetLength(1)];
+
+            for (int i = 0; i < magnitudes.GetLength(0); i++)
+            {
+                for (int j = 0; j < magnitudes.GetLength(1); j++)
+                {
+                    window.Select2D((x, y, value) =>
+                    {
+                        if (i - windowRadius + x < 0
+                            || i - windowRadius + x >= magnitudes.GetLength(0)
+                            || j - windowRadius + y < 0
+                            || j - windowRadius + y >= magnitudes.GetLength(1))
+                        {
+                            return 0;
+                        }
+
+                        return magnitudes[(int)(i - windowRadius + x), j - windowRadius + y];
+                    });
+
+                    result[i, j] = KernelHelper.Average(magnitudes) < averege ? 255 : img[i, j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
