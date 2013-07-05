9c96546241b7321c07f439ce263c3ca991349048
borisova.albina@gmail.com <borisova.albina@gmail.com@6c98e674-3fba-0178-029c-bbb524815497>
2013-07-05 07:45:01 +0000

    Updates in test for segmentation.

--- CUDAFingerprinting.Common.Segmentation.Tests/Experiment.cs
+++ CUDAFingerprinting.Common.Segmentation.Tests/Experiment.cs
@@ -13,17 +13,30 @@ namespace CUDAFingerprinting.Common.Segmentation.Tests
         [TestMethod]
         public void ExperimentMethod()
         {
-            double[,] img = ImageHelper.LoadImage(Resources._104_6);
-            double[,] resultImg;
+            double[,] img1 = ImageHelper.LoadImage(Resources._104_6);
+            double[,] img2 = ImageHelper.LoadImage(Resources._65_8);
+            double[,] img3 = ImageHelper.LoadImage(Resources._103_7);
+            double[,] resultImg1;
+            double[,] resultImg2;
+            double[,] resultImg3;
+
             int windowSize = 20;
             double minValue = 0.3;
             double maxValue = 1;
-            int threshold = 3;
-
-            for (double weight = minValue; weight <= maxValue; weight += 0.1)
+            int minThreshold = 2;
+            int maxThreshold = 7;
+            for (double weight = minValue; weight <= maxValue; weight+= 0.1)
             {
-                resultImg = Segmentator.Segmetator(img, windowSize, weight, threshold);
-                ImageHelper.SaveArray(resultImg, Path.GetTempPath() + "resultImg_104_6__" + weight + ".png");
+                for (int currentThreshold = minThreshold; currentThreshold <= maxThreshold; currentThreshold++)
+                {
+                    resultImg1 = Segmentator.Segmetator(img1, windowSize, weight, currentThreshold);
+                    ImageHelper.SaveArray(resultImg1, Path.GetTempPath() + "/104_6/resultImg_" + weight + "_" + currentThreshold + ".png");
+                    resultImg2 = Segmentator.Segmetator(img2, windowSize, weight, currentThreshold);
+                    ImageHelper.SaveArray(resultImg2, Path.GetTempPath() + "/65_8/resultImg_" + weight + "_" + currentThreshold + ".png");
+                    resultImg3 = Segmentator.Segmetator(img3, windowSize, weight, currentThreshold);
+                    ImageHelper.SaveArray(resultImg3, Path.GetTempPath() + "/103_7/resultImg_" + weight + "_" + currentThreshold + ".png");
+
+                }
             }
         }
     }
