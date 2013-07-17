2d5bbc8144b64458cc5f7f551d4e8f07356de670
chugaevatv@gmail.com <chugaevatv@gmail.com@6c98e674-3fba-0178-029c-bbb524815497>
2013-07-17 12:13:55 +0000

    Update MCC.

---
+++ CDAFingerprinting.Common.MCC/MCC.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CUDAFingerprinting.Common;
+
+namespace CDAFingerprinting.Common.MCC
+{
+    public class MCC
+    {
+        private int[,,] cuboid;
+
+        public void MCC(Minutia[] minutiae)
+        {
+           /* for (int i = 0; i < minutiae.GetLength(0); i++)
+            {
+                cuboid = new int[Constants.Ns,Constants.Ns,Constants.Nd];
+
+
+            }
+            */
+
+        }
+    }
+}
