5719c048cbb3ad1d4fd0f95b000ef358fb0a52dd
chugaevatv@gmail.com <chugaevatv@gmail.com@6c98e674-3fba-0178-029c-bbb524815497>
2013-07-17 15:26:17 +0000

    Update MCC.

--- CUDAFingerprinting.TemplateBuilding.Minutiae.MCC/MCC.cs
+++ CUDAFingerprinting.TemplateBuilding.Minutiae.MCC/MCC.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,72 +7,66 @@ using CUDAFingerprinting.Common;
 
 namespace CUDAFingerprinting.TemplateBuilding.Minutiae.MCC
 {
-    public class MCC
+public static class MCC
+{
+    private static int[, ,] value;
+    private static double deltaS;
+    private static double deltaD;
+
+    public static void MCCMethod(Minutia[] minutiae)
     {
-        private int[,,] value;
-        private double deltaS;
-        private double deltaD;
+        deltaS = 2 * Constants.R / Constants.Ns;
+        deltaD = 2 * Math.PI / Constants.Nd;
 
-        public void MCCMethod(Minutia[] minutiae)
+        for (int index = 0; index < minutiae.GetLength(0); index++)
         {
-            for (int index = 0; index < minutiae.GetLength(0); index++)
+            value = new int[Constants.Ns, Constants.Ns, Constants.Nd];
+               
+            for (int i = 0; i < Constants.Ns; i++)
             {
-                value = new int[Constants.Ns, Constants.Ns, Constants.Nd];
-                deltaS = 2 * Constants.R / Constants.Ns;
-                deltaD = 2 * Math.PI / Constants.Nd;
-
-                for (int i = 0; i < Constants.Ns; i++)
+                for (int j = 0; j < Constants.Ns; j++)
                 {
-                    for (int j = 0; j < Constants.Ns; j++)
+                    for (int k = 0; k < Constants.Nd; k++)
                     {
-                        for (int k = 0; k < Constants.Nd; k++)
-                        {
-                            //value[i, j, k] = ... ;
-                        }
+                        //value[i, j, k] = ... ;
                     }
                 }
             }
         }
+    }
 
-        private bool Psi(double value)
+    private static bool Psi(double value)
+    {
+        return value >= Constants.MuPsi;
+    }
+
+    private static Tuple<int, int> GetCoordinatesInFingerprint(Minutia m, int i, int j)
+    {
+        double halfNs = (1 + Constants.Ns)/2;
+        double sinTetta = Math.Sin(m.Angle);
+        double cosTetta = Math.Cos(m.Angle);
+        double iDelta = cosTetta*(i - halfNs) + sinTetta * (j - halfNs);
+        double jDelta = -sinTetta*(i - halfNs) + cosTetta*(j - halfNs);
+
+        return new Tuple<int, int>((int)(m.X + deltaS * iDelta), (int)(m.Y + deltaS * jDelta));
+    }
+
+    private static double GetDifferenceAngles(double tetta1, double tetta2)
+    {
+        double difference = tetta1 - tetta2;
+
+        if (difference < - Math.PI)
         {
-            return value >= Constants.MuPsi;
+            return 2*Math.PI + difference;
         }
-
-        private Tuple<int, int> GetCoordinatesInFingerprint(Minutia m, int i, int j)
-        {
-            double halfNs = (1 + Constants.Ns)/2;
-            double sinTetta = Math.Sin(m.Angle);
-            double cosTetta = Math.Cos(m.Angle);
-            double iDelta = cosTetta*(i - halfNs) + sinTetta * (j - halfNs);
-            double jDelta = -sinTetta*(i - halfNs) + cosTetta*(j - halfNs);
-
-            return new Tuple<int, int>((int)(m.X + deltaS * iDelta), (int)(m.Y + deltaS * jDelta));
-        }
-
-        private double GetDifferenceAngles(double tetta1, double tetta2)
-        {
-            double difference = tetta1 - tetta2;
-
-            if (difference < - Math.PI)
-            {
-                return 2*Math.PI + difference;
-            }
             
-            if (difference < Math.PI)
-            {
-                return difference;
-            }
-            
-            return -2*Math.PI + difference;
-        }
-
-        private double GetDirectionalContribution(Minutia m, Tuple<int, int> coordinatesInFingerprint, int k)
+        if (difference < Math.PI)
         {
-           // double angleFromLevel = GetAngleFromLevel(k);
-           // double param = GetDifferenceAngles()
-            return 0;
+            return difference;
         }
+            
+        return -2*Math.PI + difference;
+	}
 
         private double GetAngleFromLevel(int k)
         {
@@ -83,5 +78,17 @@ namespace CUDAFingerprinting.TemplateBuilding.Minutiae.MCC
             return Math.Sqrt((point1.Item1 - point2.Item1)*(point1.Item1 - point2.Item1) +(point1.Item2 - point2.Item2)*(point1.Item2 - point2.Item2));
         }
 
+    private static double GetDirectionalContribution(Minutia m, Minutia mt, Tuple<int, int> coordinatesInFingerprint, int k)
+    {
+        double angleFromLevel = GetAngleFromLevel(k);
+        double differenceAngles = GetDifferenceAngles(m.Angle, mt.Angle);
+        double param = GetDifferenceAngles(angleFromLevel, differenceAngles);
+        double factor = 1 / (Constants.SigmaD * Math.Sqrt(2 * Math.PI));
+             
+        Gd = ...
+                    
+        return 0;
     }
 }
+}
+*/
