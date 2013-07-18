13942bdba40d49aac977ad434ef561f712e6a9eb
chugaevatv@gmail.com <chugaevatv@gmail.com@6c98e674-3fba-0178-029c-bbb524815497>
2013-07-18 11:33:12 +0000

    Add MCCTest project.

---
+++ CUDAFingerprinting.TemplateBuilding.Minutiae.MCC.Test/MCCTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using CUDAFingerprinting.Common;
+using CUDAFingerprinting.TemplateBuilding.Minutiae.BinarizationThinking;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CUDAFingerprinting.TemplateBuilding.Minutiae.MCC.Test
+{
+    [TestClass]
+    public class MCCTest
+    {
+        [TestMethod]
+        public void TestMcc()
+        {
+            /*var img = ImageHelper.LoadImage(Resources._104_6);
+            double board = 5;
+            var thining = Thining.ThiningPicture(GlobalBinarization.Binarization(img, board));
+            List<Minutia> minutiaList = MinutiaeDetection.FindMinutiae(thining); */
+        }
+    }
+}
